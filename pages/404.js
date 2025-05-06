ec29bf1aa4f63e0c2f9a2a7ab265fe054e8d3ddb
Владислав Журавель <svzoo890@email.com>
2025-05-06 22:06:55 +0300

    оновив 404

--- pages/404.js
+++ pages/404.js
@@ -1,4 +1,15 @@
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 export default function Custom404() {
-    return <h1>404 - Сторінка не знайдена</h1>;
-  }
+  return (
+    <Container maxWidth="sm" sx={{ textAlign: 'center', py: 6 }}>
+      <Typography variant="h4" component="h1" gutterBottom>
+        404 - Сторінка не знайдена
+      </Typography>
+      <Typography variant="body1">
+        Вибачте, ми не змогли знайти цю сторінку.
+      </Typography>
+    </Container>
+  );
+}
